14fdae95c1bf69f7134e5ee9c960a1d3df890631
dnjswns98 <tngkrt2004@gmail.com>
2026-04-23 21:48:40 +0900

    refacotr: 필요없는 UUID 제거 및 검증 수정

--- src/main/java/org/ticketing/club/domain/model/entity/Club.java
+++ src/main/java/org/ticketing/club/domain/model/entity/Club.java
@@ -3,6 +3,9 @@ package org.ticketing.club.domain.model.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.iimsa.common.domain.BaseEntity;
+import org.iimsa.common.exception.BadRequestException;
+import org.iimsa.common.exception.ForbiddenException;
+import org.ticketing.club.domain.exception.ClubExceptionMessage;
 
 import java.util.UUID;
 
@@ -23,12 +26,14 @@ public class Club extends BaseEntity {
     private UUID adminId;
 
     @Builder(access = AccessLevel.PRIVATE)
-    private Club(UUID id, String clubName, UUID adminId) {
+    private Club(String clubName, UUID adminId) {
         this.clubName = clubName;
         this.adminId = adminId;
     }
 
     public static Club create(String clubName, UUID adminId) {
+        validate(clubName, adminId);
+
         return Club.builder()
                 .clubName(clubName)
                 .adminId(adminId)
@@ -36,11 +41,32 @@ public class Club extends BaseEntity {
     }
 
     public void updateClub(String clubName, UUID adminId) {
-        this.clubName = clubName;
-        this.adminId = adminId;
+        if (this.deletedAt != null) {
+            throw new BadRequestException(ClubExceptionMessage.ALREADY_DELETED);
+        }
+
+        if (clubName != null && !clubName.isBlank()) {
+            this.clubName = clubName;
+        }
+
+        if (adminId != null) {
+            this.adminId = adminId;
+        }
     }
 
     public void deleteClub(String deletedBy) {
+        if (this.deletedAt != null) {
+            throw new BadRequestException(ClubExceptionMessage.ALREADY_DELETED);
+        }
         super.delete(deletedBy);
+    }
+
+    private static void validate(String clubName, UUID adminId) {
+        if (clubName == null || clubName.isBlank()) {
+            throw new BadRequestException(ClubExceptionMessage.EMPTY_NAME);
+        }
+        if (adminId == null) {
+            throw new BadRequestException(ClubExceptionMessage.EMPTY_ADMIN_ID);
+        }
     }
 }
