1f31ebb88422eefafed3fa18e53f3ed75edc1adc
dnjswns98 <tngkrt2004@gmail.com>
2026-04-23 21:47:55 +0900

    feat: exception message 추가

--- src/main/java/org/ticketing/club/domain/exception/ClubExceptionMessage.java
+++ src/main/java/org/ticketing/club/domain/exception/ClubExceptionMessage.java
@@ -4,11 +4,21 @@ public final class ClubExceptionMessage {
 
     private ClubExceptionMessage() {}
 
+    // 400
+    public static final String EMPTY_NAME = "클럽명은 필수입니다.";
+    public static final String INVALID_NAME = "클럽명이 유효하지 않습니다.";
+    public static final String EMPTY_ADMIN_ID = "관리자 ID는 필수입니다.";
+    public static final String INVALID_ADMIN = "유효하지 않은 관리자입니다.";
+    public static final String CLUB_STADIUM_REQUIRED = "클럽과 경기장은 필수입니다.";
+
+    // 403
+    public static final String FORBIDDEN = "해당 클럽에 접근 권한이 없습니다.";
+    public static final String NOT_ADMIN = "관리자가 아닙니다.";
+
+    // 404
     public static final String NOT_FOUND = "클럽이 존재하지 않습니다.";
+
+    // 409
     public static final String DUPLICATE_NAME = "동일한 클럽명이 존재합니다.";
     public static final String ALREADY_DELETED = "이미 삭제된 클럽입니다.";
-
-    public static final String INVALID_ADMIN = "유효하지 않은 관리자입니다.";
-    public static final String SAME_ADMIN = "동일한 관리자로 변경할 수 없습니다.";
-    public static final String NOT_ADMIN = "클럽 관리자가 아닙니다.";
 }
--- src/main/java/org/ticketing/club/domain/exception/StadiumExceptionMessage.java
+++ src/main/java/org/ticketing/club/domain/exception/StadiumExceptionMessage.java
@@ -4,7 +4,14 @@ public final class StadiumExceptionMessage {
 
     private StadiumExceptionMessage() {}
 
+    // 400
+    public static final String EMPTY_NAME = "경기장명은 필수입니다.";
+    public static final String EMPTY_ADDRESS = "주소는 필수입니다.";
+
+    // 404
     public static final String NOT_FOUND = "경기장이 존재하지 않습니다.";
-    public static final String DUPLICATE_MAPPING = "이미 연결된 경기장입니다.";
     public static final String MAPPING_NOT_FOUND = "매핑 정보가 존재하지 않습니다.";
+
+    // 409
+    public static final String ALREADY_DELETED = "이미 삭제된 경기장입니다.";
 }
